7c6d14582f8718391b4513516b2d6f94c074acbf
henrique-code <81023332+henrique-code@users.noreply.github.com>
2025-11-27 23:24:47 -0300

    Update main.tf

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -25,9 +25,9 @@ resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
     bucket = aws_s3_bucket.static_site_bucket.id
 
     block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+    block_public_policy     = false
+	ignore_public_acls      = false
+    restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
